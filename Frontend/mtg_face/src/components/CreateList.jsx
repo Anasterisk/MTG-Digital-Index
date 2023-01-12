d605bc50bca45aad63adfa92a52509f227424ece
Alvin Toch <Alvinstoch@live.com>
2023-01-12 07:32:43 -0800

    build deployment reset

---
+++ Frontend/mtg_face/src/components/CreateList.jsx
@@ -0,0 +1,53 @@
+import axios from "axios"
+import { useState, useEffect, useContext } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+
+
+export default function EditList (){
+    const navigate = useNavigate();
+    let {id} = useParams();
+    const intialState = {
+        name:'',
+        status: '',
+        description: '',
+    }
+    const [list, setList] = useState(intialState)
+    const [form, setForm] = useState(list)
+
+        
+    const updateList = async (data)=>{
+                try{
+                    const response = await axios.post(`hhttps://p4mtg.herokuapp.com/api/lists/${id}`, data)
+                    setList(response.data)
+                    console.log(response)
+                } catch (error){
+                    throw error
+                }
+            };
+
+    
+        const handleChange = (e) =>{
+            setForm({...form, [e.target.id]:e.target.value})
+        }
+        const handleSubmit = async (e)=>{
+            e.preventDefault()
+            await updateList(form)
+            setForm(intialState)
+        }
+    
+    
+        return(
+        <div>
+        I am the Edit list
+        <form onSubmit={handleSubmit}>
+            <label> Name:</label>
+            <input type='text' id='name' placeholder={list.name} onChange ={handleChange} value={form.name}></input>
+            <label> Status:</label>
+            <input type='text' id='status' placeholder={list.status} onChange ={handleChange} value={form.status}></input>
+            <label> Description:</label>
+            <input type='text' id='description' placeholder={list.description} onChange ={handleChange} value={form.description}></input>
+            <button class="bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type='submit'>Submit</button>
+        </form>
+    </div>
+    )
+}
